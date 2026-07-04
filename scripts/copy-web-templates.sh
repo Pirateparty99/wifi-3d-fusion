d5a9e9b6d05e32aaead2c358748deaed42410588
Alejandro <29783316+Pirateparty99@users.noreply.github.com>
2026-07-03 19:00:35 -0500

    Added script to copy web template(s) for the web page/dashboard into the repo's visualization environment path.

---
+++ scripts/copy-web-templates.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script is intended to copy a custom "index.html" template to the default directory 
+# sourced for running the webserver. May include other files/templates as needed.
+
+# Add functionality to source visualization path var from the Python scripts?
+
+VISUALIZATION_PATH = "env/visualization"
+
+if [ ! -d "$VISUALIZATION_PATH" ]; then
+    mkdir -p "$VISUALIZATION_PATH"
+    echo "Created: $VISUALIZATION_PATH"
+else
+    echo "Already exists: $VISUALIZATION_PATH"
+fi
+
+copy templates/index.html $VISUALIZATION_PATH/index.html
