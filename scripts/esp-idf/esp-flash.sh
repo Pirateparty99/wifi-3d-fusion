09b922b0190495a9437c04a6c327247bdda798dc
Pirateparty99 <29783316+Pirateparty99@users.noreply.github.com>
2026-07-06 19:30:16 -0500

    Updated  ESP flash script to source the EDF-IDF venv before flashing firmware

--- scripts/esp-idf/esp-flash.sh
+++ scripts/esp-idf/esp-flash.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-idf.py flash monitor
+# ESP-IDF version 4.3 required by the ESP32 CSI Toolkit repo
+export IDF_PATH="/home/esp-idf/v4.3.3"
+
+# Set the Python venv version to one compatible for the specific ESP-IDF version (ex: Python 3.9 for ESP-IDF 4.3)
+export LEGACY_PYTHON_BIN=python3.9 
+
+# Set the board target for installing the board-specific toolchain(s) with ESP-IDF
+export ESP_TARGET=esp32,esp32c3  # multiple targets
+
+source ${IDF_PATH}/export.sh
+
+cd third_party/esp32-csi-toolkit/passive/
+# cd third_party/esp32-csi-toolkit/active_sta/
+# cd third_party/esp32-csi-toolkit/active_ap/
+idf.py flash
